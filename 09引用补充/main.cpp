f4e30b9cd7b13c889562df6f3d6b44633b5f9a4f
Silence <374619540@qq.com>
2019-09-21 18:26:44 +0800

    09引用补充

---
+++ 09引用补充/main.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+	/*int age = 10;
+	const int &rAge = age;
+	age = 30;
+
+	cout << "age is " << age << endl;
+	cout << "rAge is " <<  rAge << endl;*/
+
+	/*int age = 10;
+	const long &rAge = age;
+	age = 30;
+
+	cout << "age is " << age << endl;
+	cout << "rAge is " << rAge << endl;*/
+
+	/*int array[] = { 1, 2, 3, 4 };
+	int * const &p = array;*/
+	//int (&rArray)[4] = array;
+
+	int a = 10;
+	int b = 20;
+	// (a = b) = 30;
+	(a > b ? a : b) = 40;
+
+	cout << "a is " << a << endl;
+	cout << "b is " << b << endl;
+
+	getchar();
+
+	return 0;
+}
