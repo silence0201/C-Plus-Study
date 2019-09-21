12c6844e2ffcb8aba3215117558645667e0f3e75
Silence <374619540@qq.com>
2019-09-21 17:14:18 +0800

    03extern-C

---
+++ 03extern-C/main.cpp
@@ -0,0 +1,54 @@
+﻿#include <iostream>
+
+//extern "C" {
+#include "sum.h"
+//}
+
+// C语言库
+
+using namespace std;
+
+//extern "C" void func() {
+//	cout << "func()" << endl;
+//}
+//
+//extern "C" void func(int a) {
+//	cout << "func(int a) " << a << endl;
+//}
+
+//extern "C" {
+//	void func() {
+//		cout << "func()" << endl;
+//	}
+//
+//	void func(int a) {
+//		cout << "func(int a) " << a << endl;
+//	}
+//}
+
+//extern "C" void func();
+//extern "C" void func(int a);
+
+//extern "C" {
+//	void func();
+//	void func(int a);
+//}
+
+int main() {
+	/*func();
+	func(10);*/
+
+	cout << sum(10, 20) << endl;
+
+	getchar();
+
+	return 0;
+}
+
+//void func() {
+//	cout << "func()" << endl;
+//}
+//
+//void func(int a) {
+//	cout << "func(int a) " << a << endl;
+//}
