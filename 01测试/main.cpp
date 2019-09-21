55555b3abe9e16aaa26a78d80f9201f2cda342e9
Silence <374619540@qq.com>
2019-09-21 15:50:37 +0800

    01测试

---
+++ 01测试/main.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+using namespace std;
+
+// 在Java的世界里，先有类，再有方法（函数）
+int main() {
+
+	//cout << "Hello World!" << endl;
+
+	/*cout << "Hello";
+	cout << endl;
+	cout << "World!";*/
+
+	//cout << "Hello" << endl << "World!";
+
+	/*int age;
+	cin >> age;
+
+	cout << "age is " << age << endl;*/
+
+	// Shift + F5：退出程序
+	// Ctrl + SHift + F5：重启程序
+	// 以上快捷键只针对F5启动的程序
+
+	int a = 10;
+	int b = a >> 2;
+	// 面向对象 + 运算符（操作符）重载
+
+	// 等待键盘输入
+	getchar();
+
+	return 0;
+}
