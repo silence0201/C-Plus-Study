037978a981a871ff5990d84ef88bceceb235b2c1
Silence <374619540@qq.com>
2019-09-21 17:12:57 +0800

    02函数重载(Overload)

---
+++ 02函数重载(Overload)/main.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+using namespace std;
+
+/*
+C语言不支持函数重载
+*/
+
+// g++
+// msvc
+
+
+// display_v
+void display() {
+	cout << "display() " << endl;
+}
+
+// display_i
+void display(int a) {
+	cout << "display(int a) " << a << endl;
+}
+
+// display_l
+void display(long a) {
+	cout << "display(long a) " << a << endl;
+}
+
+// display_d
+void display(double a) {
+	cout << "display(double a) " << a << endl;
+}
+
+// 逆向工程
+int main() {
+	display();
+	display(10);
+	display(10l);
+	display(10.1);
+
+	getchar();
+
+	return 0;
+}
