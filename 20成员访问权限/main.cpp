d82c7622a22299eb9b9fd3c46348d55e7bff560b
Silence <374619540@qq.com>
2019-09-27 23:20:43 +0800

    20成员访问权限

---
+++ 20成员访问权限/main.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+using namespace std;
+
+//class Person {
+//public:
+//	int m_age;
+//	void run() {
+//
+//	}
+//};
+//
+//class Student : public Person {
+//	int m_no;
+//	void study() {
+//		m_age = 10;
+//	}
+//};
+//
+//class GoodStudent : public Student {
+//	int m_money;
+//	void work() {
+//		m_age = 10;
+//	}
+//};
+
+class Person {
+private:
+	int m_age;
+public:
+	int m_no;
+};
+
+int main() {
+	Person person;
+	person.m_no = 20;
+
+	getchar();
+	return 0;
+}
