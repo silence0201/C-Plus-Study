d2ed5743496cfa6e3df994db6e96ce82cb1d0f35
Silence <374619540@qq.com>
2019-09-27 22:16:29 +0800

    16对象的内存管理

---
+++ 16对象的内存管理/main.cpp
@@ -0,0 +1,48 @@
+﻿#include <iostream>
+using namespace std;
+
+struct Car {
+	int m_price;
+	Car() {
+		cout << "Car()" << endl;
+	}
+
+	~Car() {
+		cout << "~Car()" << endl;
+	}
+};
+
+struct Person {
+	int m_age; // 4
+	Car *m_car; // 4
+
+	// 初始化工作
+	Person() {
+		cout << "Person()" << endl;
+
+		 this->m_car = new Car();
+
+		/*Car car;
+		this->m_car = &car;*/
+	}
+
+	// 内存回收、清理工作(回收Person对象内部申请的堆空间)
+	~Person() {
+		cout << "~Person()" << endl;
+
+		delete this->m_car;
+	}
+};
+
+void test() {
+	Person *p = new Person();
+
+	delete p;
+}
+
+int main() {
+	test();
+
+	getchar();
+	return 0;
+}
