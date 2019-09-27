88374734f5f8f5c7a3e91fa841358df28d011082
Silence <374619540@qq.com>
2019-09-27 22:17:05 +0800

    17声明和实现的分离

---
+++ 17声明和实现的分离/Person.cpp
@@ -0,0 +1,21 @@
+﻿#include "Person.h"
+#include <iostream>
+using namespace std;
+
+// ::是域运算符
+// 实现 .cpp 源文件
+Person::Person() {
+	cout << "Person()" << endl;
+}
+
+Person::~Person() {
+	cout << "~Person()" << endl;
+}
+
+void Person::setAge(int age) {
+	this->m_age = age;
+}
+
+int Person::getAge() {
+	return this->m_age;
+}
---
+++ 17声明和实现的分离/main.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+#include "Person.h"
+
+using namespace std;
+
+int main() {
+
+	{
+		Person person;
+		person.setAge(20);
+		cout << person.getAge() << endl; 
+	}
+	
+	getchar();
+	return 0;
+}
