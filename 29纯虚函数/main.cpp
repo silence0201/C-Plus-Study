749fcb28110c7cd557d3e40edba59b7dcdea28d0
Silence <374619540@qq.com>
2019-09-28 13:33:45 +0800

    29纯虚函数

---
+++ 29纯虚函数/main.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+using namespace std;
+
+// 类似于Java中接口、抽象类
+// 类似于OC中的协议protocol
+
+// Animal是个抽象类
+class Animal {
+public:
+	virtual void speak() = 0;
+	virtual void run() = 0;
+};
+
+class Cat : public Animal {
+public:
+	void run() {
+
+	}
+};
+
+class WhiteCat : public Cat {
+public:
+	void speak() {
+
+	}
+	void run() {
+
+	}
+};
+
+int main() {
+	Cat *cat = new WhiteCat();
+
+	getchar();
+	return 0;
+}
