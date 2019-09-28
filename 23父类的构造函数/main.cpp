99368ce5ca6d96a4f9b1c4b8369bb15c1f02aacf
Silence <374619540@qq.com>
2019-09-28 13:23:11 +0800

    23父类的构造函数

---
+++ 23父类的构造函数/main.cpp
@@ -0,0 +1,47 @@
+﻿#include <iostream>
+using namespace std;
+
+//class Person {
+//	int m_age;
+//public:
+//	Person() {
+//		cout << "Person()" << endl;
+//	}
+//	Person(int age) :m_age(age) {
+//		cout << "Person(int age)" << endl;
+//	}
+//};
+//
+//class Student : public Person {
+//	int m_score;
+//public:
+//	Student() {
+//		cout << "Student()" << endl;
+//	}
+//	Student(int age, int score) :m_score(score), Person(age) {
+//		cout << "Student(int age, int score)" << endl;
+//	}
+//};
+
+class Person {
+	int m_age;
+public:
+	Person(int age) :m_age(age) {
+		cout << "Person(int age)" << endl;
+	}
+};
+
+class Student : public Person {
+	int m_score;
+public:
+	Student() :Person(0) {
+
+	}
+};
+
+int main() {
+	Student student;
+
+	getchar();
+	return 0;
+}
