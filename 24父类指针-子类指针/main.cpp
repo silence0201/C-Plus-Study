7ba83a4ed93c02768726928fdb7a06c45a6ef952
Silence <374619540@qq.com>
2019-09-28 13:24:20 +0800

    24父类指针-子类指针

---
+++ 24父类指针-子类指针/main.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+using namespace std;
+
+class Person {
+public:
+	int m_age;
+};
+
+// Student是一个Person
+class Student : public Person {
+public:
+	int m_score;
+};
+
+int main() {
+	// 学生是一个人
+	//Person *stu = new Student();
+	//stu->m_age = 10;
+	
+	/*Student *p = (Student *) new Person();
+	p->m_age = 10;
+	p->m_score = 20;*/
+
+	getchar();
+	return 0;
+}
