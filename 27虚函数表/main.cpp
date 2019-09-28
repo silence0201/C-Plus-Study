f5b9861f3e3655afd9558f699f746b2cc4695d3f
Silence <374619540@qq.com>
2019-09-28 13:32:31 +0800

    27虚函数表

---
+++ 27虚函数表/main.cpp
@@ -0,0 +1,43 @@
+﻿#include <iostream>
+using namespace std;
+
+class Animal {
+public:
+	int m_age;
+	virtual void speak() {
+		cout << "Animal::speak()" << endl;
+	}
+	virtual void run() {
+		cout << "Animal::run()" << endl;
+	}
+};
+
+class Cat : public Animal {
+public:
+	int m_life;
+	Cat() :m_life(0) {}
+	void speak() {
+		// 先执行父类中的成员函数
+		Animal::speak();
+
+		// 再执行自己的一些操作
+		cout << "Cat::speak()" << endl;
+	}
+	void run() {
+		cout << "Cat::run()" << endl;
+	}
+};
+
+
+int main() {
+	/*Animal *animal1 = new Animal(); 
+	animal1->speak();
+	animal1->run();*/
+
+	Animal *animal2 = new Cat();
+	animal2->speak();
+	animal2->run(); 
+
+	getchar();
+	return 0;
+}
