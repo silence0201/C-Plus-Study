1079b5cb41cc1727587e16f347906b313ce86406
Silence <374619540@qq.com>
2019-10-11 23:01:17 +0800

    43默认构造函数

---
+++ Source/43默认构造函数/main.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+using namespace std;
+
+/*
+编译器会为每个类生成一个默认的无参的构造函数
+*/
+
+class Person {
+public:
+	int m_age = 0;
+};
+
+class Student : public Person {
+
+};
+
+int main() {
+	Student student;
+
+	getchar();
+	return 0;
+}
