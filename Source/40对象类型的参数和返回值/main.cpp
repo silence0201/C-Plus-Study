227eeb48c22d353cd6ff480986a87d75546a4a72
Silence <374619540@qq.com>
2019-10-11 22:41:31 +0800

    40对象类型的参数和返回值

---
+++ Source/40对象类型的参数和返回值/main.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+using namespace std;
+
+class Car {
+	int m_price;
+public:
+	Car(int price = 0) :m_price(price) { 
+		cout << "Car(int) - " << this << " - " << this->m_price << endl;
+	}
+
+	Car(const Car &car) :m_price(car.m_price) {
+		cout << "Car(const Car &) - " << this << " - " << this->m_price << endl;
+	}
+};
+ 
+//void test1(Car &car) {
+//
+//}
+void test1(Car car) {
+
+}
+
+Car test2() {
+	Car car(10);
+	return car;
+}
+
+int main() {
+	// Car car1(10);
+	// test1(car1);
+
+	Car car2 = test2();
+
+	// Car car3;
+	// car3 = test2();
+
+
+	getchar();
+	return 0;
+}
