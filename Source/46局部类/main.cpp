059b450462c71e310d3c208d719ba3ac34cde041
Silence <374619540@qq.com>
2019-10-11 23:07:39 +0800

    46局部类

---
+++ Source/46局部类/main.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+using namespace std;
+
+int g_age = 20;
+
+void test() {
+	int age = 10;
+	static int s_age = 30;
+
+	// 局部类
+	class Person {
+	public:
+		static void run() {
+			g_age = 30;
+			s_age = 40;
+
+			cout << "run()" << endl;
+		}
+	};
+
+	Person person;
+	Person::run();
+}
+
+int main() {
+	test();
+
+	getchar();
+	return 0;
+}
