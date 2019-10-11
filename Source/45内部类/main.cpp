0205ce54128b1411464c6ef632bdddc2dd1d3726
Silence <374619540@qq.com>
2019-10-11 23:05:48 +0800

    45内部类

---
+++ Source/45内部类/main.cpp
@@ -0,0 +1,64 @@
+﻿#include <iostream>
+using namespace std;
+
+// Person
+class Person {
+private:
+	static int ms_legs;
+	static void other() {
+
+	}
+	int m_age;
+	void walk() {
+
+	}
+
+	// Car
+	class Car {
+		int m_price;
+	public:
+		Car() {
+			cout << "Car()" << endl;
+		}
+
+		void run() {
+			Person person;
+			person.m_age = 10;
+			person.walk();
+
+			ms_legs = 10;
+			other();
+		}
+	};
+public:
+	Person() {
+		cout << "Person()" << endl;
+
+		Car car;
+	}
+
+	
+};
+
+
+int Person::ms_legs = 2;
+
+class Point {
+	class Math {
+		void test();
+	};
+};
+
+void Point::Math::test() {
+
+}
+
+int main() {
+	cout << sizeof(Person) << endl;
+	Person person;
+
+	Person::Car car;
+
+	getchar();
+	return 0;
+}
