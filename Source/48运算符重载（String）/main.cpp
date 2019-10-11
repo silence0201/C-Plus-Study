557ee742334268d3f6b76a294a12b8ed6d9ae5a3
Silence <374619540@qq.com>
2019-10-11 23:09:06 +0800

    48运算符重载（String）

---
+++ Source/48运算符重载（String）/main.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+#include "String.h"
+using namespace std;
+
+int main() {
+
+	{
+		String str1 = "1111";
+		String str2 = "2222";
+
+		// ASCIIֵ
+		// strcmp
+
+		/*if (str2 > str1) {
+
+		}
+		if (str2 == str1) {
+
+		}
+		strcat
+		String str3 = str1 + str2;
+		str1 += str2;*/
+
+		/*String str2 = "2222";
+
+		str1 = "324234";
+		cout << str1 << endl;
+
+		str1 = str1;
+		cout << str1 << endl;
+
+		str1 = str2;
+		cout << str1 << endl; */
+	}
+
+	getchar();
+	return 0;
+}
