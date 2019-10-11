81725be03913a0763d1953e9d8914171b6c7d090
Silence <374619540@qq.com>
2019-10-11 23:09:34 +0800

    49运算符重载（String）

---
+++ Source/49运算符重载（String）/main.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+#include "String.h"
+using namespace std;
+
+int main() {
+	{
+		String str1 = "abc";
+		cout << (str1 > "aac") << endl;
+	}
+
+	// str1 = 12234
+
+	getchar();
+	return 0;
+}
