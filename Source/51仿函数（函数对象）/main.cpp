8d1f0c076fad3631bb5f445123891f2192dadaf4
Silence <374619540@qq.com>
2019-10-13 13:23:26 +0800

    51仿函数（函数对象）

---
+++ Source/51仿函数（函数对象）/main.cpp
@@ -0,0 +1,53 @@
+﻿#include <iostream>
+using namespace std;
+
+//int sum(int a, int b) {
+//	return a + b;
+//}
+
+class Sum {
+	int m_age;
+public:
+	Sum(int age) :m_age(age) { }
+	int operator()(int a, int b) {
+		if (this->m_age > 10) {
+		}
+		else {
+		}
+		return a + b;
+	}
+};
+
+class Point {
+	friend ostream &operator<<(ostream &, const Point &);
+public:
+	int m_x;
+	int m_y;
+	Point(int x, int y) :m_x(x), m_y(y) { }
+
+
+};
+
+// output stream
+ostream &operator<<(ostream &cout, const Point &point) {
+	return cout << "(" << point.m_x << ", " << point.m_y << ")";
+}
+
+// input stream
+istream &operator>>(istream &cin, Point &point) {
+	return cin >> point.m_x >> point.m_y;
+}
+
+int main() {
+	Point p1(10, 20);
+	cin >> p1;
+	cout << p1 << endl;
+
+	// Sum sum(20);
+
+	// cout << sum(10, 20) << endl;
+	// cout << sum.operator()(10, 20) << endl;
+	getchar();
+	getchar();
+	return 0;
+}
