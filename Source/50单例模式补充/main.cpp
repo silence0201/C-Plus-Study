409d7bed0c47c47d00992d7a820eb8be6eb23add
Silence <374619540@qq.com>
2019-10-13 13:22:15 +0800

    50单例模式补充

---
+++ Source/50单例模式补充/main.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+using namespace std;
+class Rocket {
+public:
+	static Rocket * sharedRocket() {
+		if (ms_rocket) return ms_rocket;
+		return ms_rocket = new Rocket();
+	}
+	static void deleteRocket() {
+		if (!ms_rocket) return;
+
+		delete ms_rocket;
+		ms_rocket = NULL;
+	}
+private:
+	static Rocket *ms_rocket;
+	Rocket() { }
+	Rocket(const Rocket &rocket) { }
+	void operator=(const Rocket &rocket) { }
+};
+
+Rocket *Rocket::ms_rocket = NULL;
+
+/*
+单例需要禁止掉：拷贝行为、赋值行为
+*/
+
+int main() {
+
+	Rocket *p1 = Rocket::sharedRocket();
+	Rocket *p2 = Rocket::sharedRocket();
+
+	getchar();
+	return 0;
+}
