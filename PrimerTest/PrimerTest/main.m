5d789946fb505ee76d8a3cfe9ac8144a4b5e1df4
Silence <374619540@qq.com>
2019-10-27 12:26:51 +0800

    Update

---
+++ PrimerTest/PrimerTest/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  PrimerTest
+//
+//  Created by Silence on 2019/10/27.
+//  Copyright © 2019年 Silence. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
